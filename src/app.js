4d240ac9e0ff59e3742ee69eeced70e6a253d23c
xtadeushx <alekscherkasov13@gmail.com>
2022-09-01 19:01:21 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const a = 1;
+
+function fa(name) {
+  return name;
+}
+
+const x = 200;
+
+console.log(fa);
+console.log(x);
+console.log(window);
+console.log(a);
+
+
+const user = {
+  name: "Ura"
+}
+
+const name = user.name
