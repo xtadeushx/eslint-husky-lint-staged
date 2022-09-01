6a07bc2ad88453eb48949904ed5047663a06061f
xtadeushx <alekscherkasov13@gmail.com>
2022-09-01 19:07:35 +0300

    thierd  commit

--- src/app.js
+++ src/app.js
@@ -6,15 +6,16 @@ function fa(name) {
 
 const x = 200;
 
-console.log(fa);
-console.log(x);
-console.log(window);
+console.log(fa)
+console.log(x)
+console.log(window)
 console.log(a);
 
 
 const user = {
-  name: "Ura",
+  name: 'Ura',
 	age : 28,
 }
 
 const name = user.name
+const age = user.age
